997473dae260f7a83e3c1e23492a0dbc17e0705d
Hendrik F <hendriklast@hotmail.com>
2016-10-31 15:48:55 -0200

    Create 1002.c

---
+++ 1002.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main ()
+{
+
+ double pi, r, a;
+
+ pi = 3.14159;
+
+ scanf("%lf", &r);
+
+ a = pi * pow(r,2);
+
+ printf("A=%.4lf\n", a);
+
+
+ return 0;
+
+}
